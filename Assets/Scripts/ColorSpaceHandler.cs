672e182319441257734f84414395f7e7421ec25c
Rezmason <jeremysachs@rezmason.net>
2016-09-30 20:45:58 -0700

    Dynamic mesh generation on the Ribbon mesh filter.

--- Assets/Scripts/ColorSpaceHandler.cs
+++ Assets/Scripts/ColorSpaceHandler.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[SelectionBase]
 public class ColorSpaceHandler : MonoBehaviour {
 	void Start () {
 		Shader.SetGlobalMatrix("_InvertedColorSpaceTransform", transform.worldToLocalMatrix);

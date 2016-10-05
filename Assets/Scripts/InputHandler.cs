5448497091d1b6a25947f6bdfc1dd6cd9acddc29
Rezmason <jeremysachs@rezmason.net>
2016-10-05 06:48:34 -0700

    Added pause mode. Added easing to flight controls and speed changes.

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -3,25 +3,43 @@ using System.Collections;
 
 public class InputHandler : MonoBehaviour {
 
+	const float SPEED_CHANGE_RATIO = 0.99f;
+	Vector3 eulerAngles;
+	float speed;
+	bool paused;
+
 	// Use this for initialization
 	void Start() {
 		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+		speed = 100;
 	}
 	
 	// Update is called once per frame
 	void Update() {
 		float delta = Time.deltaTime;
-		float speed = Input.GetKey("space") ? 200 : 100;
-		transform.position += transform.forward * delta * speed;
-		UpdateNaiveControlScheme(delta);
+
+		if (Input.GetKeyDown("escape")) {
+			paused = !paused;
+		}
+
+		if (!paused) {
+			speed = speed * SPEED_CHANGE_RATIO + (Input.GetKey("space") ? 200 : 100) * (1 - SPEED_CHANGE_RATIO);
+			transform.position += transform.forward * delta * speed;
+			UpdateNaiveControlScheme(delta);
+		}
 	}
 
 	void UpdateNaiveControlScheme(float delta) {
-		Vector3 eulerAngles = new Vector3(
-          -Input.GetAxis("Mouse Y") * 50, 
-          Input.GetAxis("Mouse X") * 100, 
+
+		eulerAngles += new Vector3(
+          -Input.GetAxis("Mouse Y") * 40, 
+          Input.GetAxis("Mouse X") * 80, 
           Input.GetAxis("Mouse X") * -50
       	);
+
+		eulerAngles *= 0.95f;
+
 		transform.Rotate(eulerAngles * delta);
 	}
 }

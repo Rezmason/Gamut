fcd21731283ac051374c1727f1692df9f4454d2c
Rezmason <jeremysachs@rezmason.net>
2016-10-10 23:15:37 -0700

    Getting back into the swing of things.
    Created EntryPointHandler, which initializes a Context.

---
+++ Assets/Scripts/EntryPointHandler.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class EntryPointHandler : MonoBehaviour {
+	void Start () {
+		Context.Init(gameObject);
+	}
+}
---
+++ Assets/src/Context.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Context
+{
+	static Scene scene;
+
+	public static void Init(GameObject gameObject)
+	{
+		CheckpointSystem.instance.Init();
+		// TODO: populate scene from prefabs
+	}
+}
